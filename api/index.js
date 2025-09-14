e26cb055a1653e6f30676cf5d807cc31ac67344b
ALVIN GEORGE <Alvingeorge_@outlook.com>
2025-09-14 23:55:28 +0530

    Add CORS support to API and update dependencies

--- api/index.js
+++ api/index.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const app = express();
+import cors from "cors";
 
 // Import products from same folder
 const products = require('./products');
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.json(products);
 });
