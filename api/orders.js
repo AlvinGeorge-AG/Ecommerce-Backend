ff0b77d7e88ecffdfec9daf1b68d6410e8fa050f
ALVIN GEORGE <Alvingeorge_@outlook.com>
2025-09-15 22:41:28 +0530

    date added

--- api/orders.js
+++ api/orders.js
@@ -3,19 +3,22 @@ const orders = [
         name: "Wireless Headphones",
         orderAmount: 120.50,
         quantity: 3,
-        arrivingOn: "2024-07-10"
+        arrivingOn: "2024-07-10",
+        date:"October 12"
     },
     {
         name: "Bluetooth Speaker",
         orderAmount: 75.00,
         quantity: 1,
-        arrivingOn: "2024-07-12"
+        arrivingOn: "2024-07-12",
+        date:"March 13"
     },
     {
         name: "Smart Watch",
         orderAmount: 200.99,
         quantity: 5,
-        arrivingOn: "2024-07-15"
+        arrivingOn: "2024-07-15",
+        date:"June 10"
     }
 ];
 
