c9323d4b3793f21492c3ee128428d10e619003f6
ALVIN GEORGE <Alvingeorge_@outlook.com>
2025-09-15 09:18:22 +0530

    Add cart data and implement cart endpoint in API

--- api/index.js
+++ api/index.js
@@ -4,9 +4,15 @@ import cors from "cors";
 
 // Import products from same folder
 const products = require('./products');
+const cartdata = require("./cart");
 app.use(cors());
-app.get('/', (req, res) => {
+
+app.get('/products', (req, res) => {
   res.json(products);
 });
+
+app.get('/cart',(req,res)=>{
+  res.json(cartdata);
+})
 
 module.exports = app;
