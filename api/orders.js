a8ec9eea1f9cb3fe5529ce2ee67f07ee1bbb7772
ALVIN GEORGE <Alvingeorge_@outlook.com>
2025-09-15 22:46:33 +0530

    image added

--- api/orders.js
+++ api/orders.js
@@ -4,21 +4,24 @@ const orders = [
         orderAmount: 120.50,
         quantity: 3,
         arrivingOn: "2024-07-10",
-        date:"October 12"
+        date:"October 12",
+        image : "artistic-bowl-set-6-piece.jpg"
     },
     {
         name: "Bluetooth Speaker",
         orderAmount: 75.00,
         quantity: 1,
         arrivingOn: "2024-07-12",
-        date:"March 13"
+        date:"March 13",
+        image : "images/products/athletic-cotton-socks-6-pairs.jpg"
     },
     {
         name: "Smart Watch",
         orderAmount: 200.99,
         quantity: 5,
         arrivingOn: "2024-07-15",
-        date:"June 10"
+        date:"June 10",
+        image : "bathroom-mat.jpg"
     }
 ];
 
