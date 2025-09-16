1615535b0bf8f31877b84960c06f8d15a13e9662
ALVIN GEORGE <Alvingeorge_@outlook.com>
2025-09-16 09:04:25 +0530

    addeda post

--- api/index.js
+++ api/index.js
@@ -1,12 +1,23 @@
 const express = require('express');
 const app = express();
-import cors from "cors";
+
+const cors = require("cors"); 
 
 // Import products from same folder
 const products = require('./products');
 const cartdata = require("./cart");
 const orders = require("./orders");
+
+
 app.use(cors());
+
+
+// Middleware to parse JSON body
+app.use(express.json());
+
+// Middleware to parse URL-encoded form data
+app.use(express.urlencoded({ extended: true }));
+
 
 app.get('/products', (req, res) => {
   res.json(products);
@@ -19,5 +30,12 @@ app.get('/cart',(req,res)=>{
 app.get('/orders',(req,res)=>{
   res.json(orders);
 })
+
+app.post('/addtocart',(req,res)=>{
+  console.log(req.body);
+  res.json({ message: "Data received successfully!", data: req.body });
+})
+
+
 
 module.exports = app;
